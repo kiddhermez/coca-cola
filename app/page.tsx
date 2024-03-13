eabf8881b29e2d7d9f03f6ef8b7759d0c1b94a14
Maria Paula <malyx04@gmail.com>
2024-03-13 16:37:55 -0500

    Se agrego un boton de AÑADIR

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import Navbar from '@/components/navbar';
 
 export default function Home() {
     return <main className='bg-coke-background h-screen w-screen flex flex-col items-center py-5'>
-        <Navbar/>
+        <Navbar />
+        <div className="flex w-full justify-start py-10 px-10 gap-5">
+            <button className="rounded-full bg-coke-accent py-3 px-6">
+                Añadir item
+            </button>
+        </div>
     </main>;
 }
