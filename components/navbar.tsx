a01347853bce98d6942317c8de85d2a2a3110932
Maria Paula <malyx04@gmail.com>
2024-03-21 16:37:29 -0500

    añadir boton de inicio de sesion

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,12 +5,13 @@ import Logo from './logo'
 
 function Navbar() {
   return (
-    <nav className='w-[95%] py-5 px-10 rounded-xl bg-coke-black text-coke-foreground bg-coke-accent bg-opacity-30 flex justify-between'>
+    <nav className='w-[95%] py-5 px-10 rounded-xl bg-coke-black text-coke-foreground bg-coke-accent bg-opacity-30 flex justify-between items-center'>
       <h1 className='font-bold'>
-        <Logo className='w-[20%]' fill='#ffffff'/>
-        </h1>
-        <div className=''>Inventario</div>
-        </nav>
+        <Logo className='w-[20%]' fill='#ffffff' />
+      </h1>
+      <div className='mr-52'>Inventario</div>
+      <button className='hover:opacity-80 bg-black/20 px-3 py-2 rounded-full'>Iniciar sesion</button>
+    </nav>
   )
 }
 
