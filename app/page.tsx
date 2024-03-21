9b6ff9ccf8f5c9e33ee65823b65707ff32259bd8
sanabria <161388004+juliansanabriaR@users.noreply.github.com>
2024-03-21 16:51:26 -0500

    Añadir boton para ver transportistas

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,24 @@ import Navbar from '@/components/navbar';
 export default function Home() {
     return <main className='bg-coke-background h-screen w-screen flex flex-col items-center py-5'>
         <Navbar />
-        <div className="flex w-full justify-start py-10 px-10 gap-5">
-            <button className="rounded-full bg-coke-accent py-3 px-6">
-                Añadir item
-            </button>
-            <button className="rounded-full bg-coke-accent py-3 px-6  ">
-                Eliminar item.
-            </button>
-            <button className="rounded-full bg-coke-accent py-3 px-6  ">
-                Editar item
-            </button>
-        </div>
+        <section className='flex justify-between items-center py-10 px-10 w-full'>
+            <div className="flex w-full justify-start gap-5">
+                <button className="rounded-full bg-coke-accent py-3 px-6">
+                    Añadir item
+                </button>
+                <button className="rounded-full bg-coke-accent py-3 px-6  ">
+                    Eliminar item.
+                </button>
+                <button className="rounded-full bg-coke-accent py-3 px-6  ">
+                    Editar item
+                </button>
+            </div>
+            <div>
+                <button className="rounded-full bg-coke-accent py-3 px-6  ">
+                    Ver Transportistas
+                </button>
+            </div>
+        </section>
+
     </main>;
 }
