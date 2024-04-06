2d07a1d7bf62aec6229d1c89e2bebf04120c2e7b
kiddhermez <dayvidnavarreteqgmail.com>
2024-04-06 01:28:02 -0500

    Add nextUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,32 @@
 import type { Metadata } from 'next';
 import { Poppins as Font } from 'next/font/google';
 import './globals.css';
+import { Providers } from './providers';
 
-const font = Font({ weight: ['400'], subsets:["latin"] });
+const font = Font({ weight: ['400'], subsets: ['latin'] });
+
+// Awesome Icons
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fab, far, fas);
 
 export const metadata: Metadata = {
-    title: 'Coca-Cola app',
-    description: "An app to manage the coca-cola's inventory",
+  title: 'Coca-Cola app',
+  description: "An app to manage the coca-cola's inventory",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang='en'>
-            <body className={font.className}>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en" className="dark">
+      <body className={font.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import { nextui } from '@nextui-org/react';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-  extend:{
-    colors:{
-      coke:{
-        accent: "#cc444b",
-        background: "#38040e",
-        foreground: "#edf2f4",
-      }
-    }
-  }
+    extend: {
+      colors: {
+        coke: {
+          accent: '#cc444b',
+          background: '#38040e',
+          foreground: '#edf2f4',
+        },
+      },
+    },
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [nextui(), require('tailwind-scrollbar')],
 };
 export default config;
